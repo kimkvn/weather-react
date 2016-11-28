2ac8164c8a23dad0eccb2aabdbce9d90ad6b8247
kimkvn <kimkvn@gmail.com>
2016-11-28 16:58:12 -0500

    maybe because I was misplacing the arrow function, or maybe fetch doesn't play nice with arrow functions but aliasing this as component worked just a well. Wrote method to convert Kelvin temp to F and C

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,13 @@
 var App = React.createClass({
 
+  getInitialState: function(){
+    return({
+      tempKel: '',
+      tempF: '',
+      tempC: '',
+    });
+  },
+
   componentDidMount: function(){
     var weatherKey = '8ef93b3325212e85f516fd49083dd5f0';
     var latitude = 33.748995;
@@ -7,6 +15,8 @@ var App = React.createClass({
     var weatherURL = '//api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID='+weatherKey;
 
     // https://api.darksky.net/forecast/9015e70a6b3a67646b7b52980ff99846/33.748995,-84.387982
+
+    var component = this;
 
     fetch(weatherURL)
       .then(
@@ -18,6 +28,11 @@ var App = React.createClass({
           //Examine the text in the response;
           response.json().then(function(data){
             console.log(data);
+            console.log(data.main.temp);
+            component.setState({
+              tempKel: data.main.temp
+            });
+            component.tempConvert();
           });
         }
       )
@@ -25,24 +40,31 @@ var App = React.createClass({
         console.log('Fetch Error', err);
       });
 
-    // $.ajax({
-    //   url: weatherURL,
-    //   type: 'GET',
-    //   data: {},
-    //   dataType: 'json',
-    //   success: (data) => {
-    //     console.log(data)
-    //   },
-    //   error: function(err){
-    //     console.log('Whoops, error:'+err)
-    //   }
-    // })
+      // IMPORTANT!!!!
+      //    the default unit returned for temperature is in Kelvin.
 
+  },
+
+  tempConvert: function(){
+    var kelvin = this.state.tempKel;
+    var fahrenheit = Math.floor(kelvin * (9/5) - 459.67);
+    var celsius = Math.floor(kelvin - 273.15);
+    this.setState({
+      tempF: fahrenheit,
+      tempC: celsius,
+    });
   },
 
   render: function(){
     return(
-      <h1>hello world</h1>
+      <div>
+        <h1>hello world</h1>
+        <div>
+          The current temperature is:
+          <h3>{this.state.tempF} F</h3>
+          <h3>{this.state.tempC} C</h3>
+        </div>
+      </div>
   )}
 });
 
