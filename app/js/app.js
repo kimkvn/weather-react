b470c60bb7e4005157da00cfa5a65c9df814fc10
kimkvn <kimkvn@gmail.com>
2016-11-28 14:03:46 -0500

    running into a CORs issue trying to talk to the Dark Sky api. Wow I just realized how edgy that name sounds. Anyways, attempting to research the matter further, one example required me to update node

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,39 @@
 var App = React.createClass({
 
+  componentDidMount: function(){
+    var weatherKey = '9015e70a6b3a67646b7b52980ff99846';
+    var latitude = 33.748995;
+    var longitude = -84.387982;
+    var weatherURL = 'https://api.darksky.net/forecast/'+weatherKey+'/'+latitude+','+longitude ;
+
+    // fetch(weatherURL)
+    //   .then(
+    //     function(response){
+    //       if(response.status !== 200){
+    //         console.log('Whoops, encountered issue. Status Code:'+response.status);
+    //         return;
+    //       }
+    //       //Examine the text in the response;
+    //       response.json().then(function(data){
+    //         console.log(data);
+    //       });
+    //     }
+    //   )
+    //   .catch(function(err){
+    //     console.log('Fetch Error', err);
+    //   });
+
+    fetchJsonp(weatherURL)
+      .then(function(response){
+        return response.json()
+      }).then(function(json){
+        console.log('parsed json', json)
+      }).catch(function(ex){
+        console.log('parsing failed', ex)
+      });
+
+  },
+
   render: function(){
     return(
       <h1>hello world</h1>
