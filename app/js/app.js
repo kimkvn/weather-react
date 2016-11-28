068fbb3e45ff656f0186c803b5b0683733a3953e
kimkvn <kimkvn@gmail.com>
2016-11-28 14:27:45 -0500

    ALLLLRIGHT, so I got tired of trying to find a solution to using the dark sky API. I thought this was the recommended API from FCC but as it turns out, they suggest using api.openweathermap.org, which, surprise, is working for me. MOVING ON.

--- app/js/app.js
+++ app/js/app.js
@@ -1,36 +1,42 @@
 var App = React.createClass({
 
   componentDidMount: function(){
-    var weatherKey = '9015e70a6b3a67646b7b52980ff99846';
+    var weatherKey = '8ef93b3325212e85f516fd49083dd5f0';
     var latitude = 33.748995;
     var longitude = -84.387982;
-    var weatherURL = 'https://api.darksky.net/forecast/'+weatherKey+'/'+latitude+','+longitude ;
+    var weatherURL = '//api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID='+weatherKey;
 
-    // fetch(weatherURL)
-    //   .then(
-    //     function(response){
-    //       if(response.status !== 200){
-    //         console.log('Whoops, encountered issue. Status Code:'+response.status);
-    //         return;
-    //       }
-    //       //Examine the text in the response;
-    //       response.json().then(function(data){
-    //         console.log(data);
-    //       });
-    //     }
-    //   )
-    //   .catch(function(err){
-    //     console.log('Fetch Error', err);
-    //   });
+    // https://api.darksky.net/forecast/9015e70a6b3a67646b7b52980ff99846/33.748995,-84.387982
 
-    fetchJsonp(weatherURL)
-      .then(function(response){
-        return response.json()
-      }).then(function(json){
-        console.log('parsed json', json)
-      }).catch(function(ex){
-        console.log('parsing failed', ex)
+    fetch(weatherURL)
+      .then(
+        function(response){
+          if(response.status !== 200){
+            console.log('Whoops, encountered issue. Status Code:'+response.status);
+            return;
+          }
+          //Examine the text in the response;
+          response.json().then(function(data){
+            console.log(data);
+          });
+        }
+      )
+      .catch(function(err){
+        console.log('Fetch Error', err);
       });
+
+    // $.ajax({
+    //   url: weatherURL,
+    //   type: 'GET',
+    //   data: {},
+    //   dataType: 'json',
+    //   success: (data) => {
+    //     console.log(data)
+    //   },
+    //   error: function(err){
+    //     console.log('Whoops, error:'+err)
+    //   }
+    // })
 
   },
 
