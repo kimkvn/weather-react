410af367f7e51b3d754337fe07f2aadb38e621fe
kimkvn <kimkvn@gmail.com>
2016-12-01 16:50:01 -0500

    styling mobile view, 3 cards representing the forecast stacked on each other. Positioning the highs and lows, what a pain

--- app/js/forecast.js
+++ app/js/forecast.js
@@ -82,39 +82,39 @@ var Forecast = React.createClass({
 
   render: function(){
     return(
-      <div className="forecast-wrap">
-        <div className="day-0">
-          <div className="day-name">
+      <div className="forecast-wrap row">
+        <div className="day-0 day-block col-xs-12 col-sm-4">
+          <div className="day-name col-xs-4">
             <h3>{this.state.day0}</h3>
           </div>
-          <div className="img-wrap">
+          <div className="img-wrap col-xs-4">
             <img src={this.state.day0Icon} />
           </div>
-          <div className="high-low">
+          <div className="high-low col-xs-4">
             <h5 className="high">{this.state.day0High}</h5>
             <h5 className="low">{this.state.day0Low}</h5>
           </div>
         </div>
-        <div className="day-1">
-          <div className="day-name">
+        <div className="day-1 day-block col-xs-12 col-sm-4">
+          <div className="day-name col-xs-4">
             <h3>{this.state.day1}</h3>
           </div>
-          <div className="img-wrap">
+          <div className="img-wrap col-xs-4">
             <img src={this.state.day1Icon} />
           </div>
-          <div className="high-low">
+          <div className="high-low col-xs-4">
             <h5 className="high">{this.state.day1High}</h5>
             <h5 className="low">{this.state.day1Low}</h5>
           </div>
         </div>
-        <div className="day-2">
-          <div className="day-name">
+        <div className="day-2 day-block col-xs-12 col-sm-4">
+          <div className="day-name col-xs-4">
             <h3>{this.state.day2}</h3>
           </div>
-          <div className="img-wrap">
+          <div className="img-wrap col-xs-4">
             <img src={this.state.day2Icon} />
           </div>
-          <div className="high-low">
+          <div className="high-low col-xs-4">
             <h5 className="high">{this.state.day2High}</h5>
             <h5 className="low">{this.state.day2Low}</h5>
           </div>
