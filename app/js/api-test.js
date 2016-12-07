1727385e704bda658d93bd18a0583a09b86eb877
kimkvn <kimkvn@gmail.com>
2016-12-07 17:58:04 -0500

    YEAH. Reading through some of the documented issues on fetch-jsonp, and following the examples written by @camsong, I was able to successfully make a call to the DarkSky API. Yay.

---
+++ app/js/api-test.js
@@ -0,0 +1,47 @@
+var ApiTest = React.createClass({
+
+  componentDidMount: function(){
+    var latitude = 33.748995;
+    var longitude = -84.387982;
+    var key = '9015e70a6b3a67646b7b52980ff99846';
+    var weatherURL = 'https://api.darksky.net/forecast/'+ key + '/'+latitude+','+longitude
+
+    // fetchJsonp(weatherURL)
+    //   .then(
+    //     function(response){
+    //       if(response.status !== 200){
+    //         console.log('Whoops! Encountered issue. Status Code:'+response.status);
+    //         return;
+    //       }
+    //
+    //       response.json().then(function(data){
+    //         console.log(data);
+    //       })
+    //     }
+    //   )
+    //   .catch(function(err){
+    //     console.log('Fetch Error', err);
+    //   })
+
+    var result = fetchJsonp(weatherURL, {
+      // jsonpCallback: 'jsoncallback',
+      timeout: 3000
+    })
+    result.then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      document.body.innerHTML = JSON.stringify(json);
+      console.log(json)
+    })['catch'](function(ex) {
+      document.body.innerHTML = 'failed:' + ex;
+    })
+
+
+  },
+
+  render: function(){
+    return <h1>hello world</h1>
+  }
+});
+
+ReactDOM.render(<ApiTest />, document.getElementById('api-test'));
