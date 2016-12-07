62e70042645a52347f08629dc5485a76c4cb61c6
kimkvn <kimkvn@gmail.com>
2016-12-07 18:21:09 -0500

    ohkay. So including the if statement to log a status error message makes fetch-jsonp not work. Noted...

--- app/js/api-test.js
+++ app/js/api-test.js
@@ -6,36 +6,36 @@ var ApiTest = React.createClass({
     var key = '9015e70a6b3a67646b7b52980ff99846';
     var weatherURL = 'https://api.darksky.net/forecast/'+ key + '/'+latitude+','+longitude
 
-    // fetchJsonp(weatherURL)
-    //   .then(
-    //     function(response){
-    //       if(response.status !== 200){
-    //         console.log('Whoops! Encountered issue. Status Code:'+response.status);
-    //         return;
-    //       }
-    //
-    //       response.json().then(function(data){
-    //         console.log(data);
-    //       })
-    //     }
-    //   )
-    //   .catch(function(err){
-    //     console.log('Fetch Error', err);
-    //   })
-
-    var result = fetchJsonp(weatherURL, {
-      // jsonpCallback: 'jsoncallback',
+    fetchJsonp(weatherURL, {
       timeout: 3000
     })
-    result.then(function(response) {
-      return response.json()
-    }).then(function(json) {
-      document.body.innerHTML = JSON.stringify(json);
-      console.log(json)
-    })['catch'](function(ex) {
-      document.body.innerHTML = 'failed:' + ex;
-    })
+      .then(
+        function(response){
+          // if(response.status !== 200){
+          //   console.log('Whoops! Encountered issue. Status Code:'+response.status);
+          //   return;
+          // }
+          response.json().then(function(data){
+            console.log(data)
+          })
+        })
+      .catch(function(err){
+        console.log('Fetch Error:', err);
+      })
 
+  //   var result = fetchJsonp(weatherURL, {
+  //     // jsonpCallback: 'jsoncallback',
+  //     timeout: 3000
+  //   })
+  //   result.then(function(response) {
+  //     return response.json()
+  //   }).then(function(data) {
+  //     // document.body.innerHTML = JSON.stringify(json);
+  //     console.log(data)
+  //   }).catch(
+  //     function(err) {
+  //       console.log('Fetch error:' + err);
+  //     });
 
   },
 
