815d9cc0f1107b8c2909a95268d6ef1c1dc2d093
kimkvn <kimkvn@gmail.com>
2016-12-08 10:17:28 -0500

    adding some exclusions to the darksky API url because...I don't think I need hourly, minutely, or flags for my purposes. Also readibility.

--- app/js/api-test.js
+++ app/js/api-test.js
@@ -4,7 +4,7 @@ var ApiTest = React.createClass({
     var latitude = 33.748995;
     var longitude = -84.387982;
     var key = '9015e70a6b3a67646b7b52980ff99846';
-    var weatherURL = 'https://api.darksky.net/forecast/'+ key + '/'+latitude+','+longitude
+    var weatherURL = 'https://api.darksky.net/forecast/'+ key + '/'+latitude+','+longitude+'/?exclude=hourly,minutely,flags'
 
     fetchJsonp(weatherURL, {
       timeout: 3000
@@ -14,6 +14,11 @@ var ApiTest = React.createClass({
       })
       .then(function(data){
         console.log(data)
+
+        //note: data.daily.time is a unix time stamp - multiply this value by 1000
+        //to get milliseconds, which you can then new Date() and get
+        //get the correct response
+
       })
       .catch(function(err){
         console.log('Fetch Error:', err);
